6c7f6de88d4a2e8a85221b317d0795b05a03b032
Cosmo Myzrail Gorynych <cosmomyzrailgorynych@gmail.com>
2025-08-06 23:13:59 +1200

    :zap: Remove dependence on Vercel, build a static site

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <div class="flex h-full w-full flex-col items-center justify-center">
-	<p>Created by Chris.</p>
+	<p>Created by <a href="https://github.com/lcmchris" target="_blank">Chris</a>. Forked by <a href="https://github.com/CosmoMyzrailGorynych">Cosmo Myzrail Gorynych</a></p>
 	<a href="/" use:shortcuts={{ keys: ['ArrowLeft'] }}>back</a>
 </div>
